0baac52dcda31ede50059b2ea4e92427f0d07664
saud maruli <uthrul@gmail.com>
2019-01-28 10:38:25 +0700

    basic Redis database

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-# golang-webapp
+# web go
 Writing a Web Application in Go.
+=======================================
+
+0. pck
+package main
+
+func main() {
+	
+}
 
 1. install gorilla mux.
 - go get -u github.com/gorilla/mux
+
+2. for Redis database
+- go get -u github.com/go-redis/redis
+- brew install redis
+- run server : redis-server & redis-cli
 
 
--- main.go
+++ main.go
@@ -4,19 +4,28 @@ import (
 	"html/template"
 	"net/http"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 )
 
+var client *redis.Client
 var templates *template.Template
 
 func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/", goHandler).Methods("GET")
+	r.HandleFunc("/", indexHandler).Methods("GET")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-func goHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "go.html", nil)
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
 }
